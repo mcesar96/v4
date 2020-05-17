8297479a57c0d5e1b3a6a36358d5972cea12ab31
Marcus Cesar <marcuscesar1996@gmail.com>
2020-05-17 11:15:42 -0500

    name change

--- src/pages/archive.js
+++ src/pages/archive.js
@@ -109,7 +109,7 @@ const ArchivePage = ({ location, data }) => {
   return (
     <Layout location={location}>
       <Helmet>
-        <title>Archive | Brittany Chiang</title>
+        <title>Archive | Marcus Cesar</title>
         <link rel="canonical" href="https://marcuscesar.com/archive" />
       </Helmet>
 
